37870794f897cc17025339b631385f2516238124
wgb <wbecker@roomkey.com>
2015-03-18 10:58:28 -0400

    Bump to latest solr 4.x

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.solr/solr-core "4.9.0"]
-                 [org.apache.solr/solr-solrj "4.9.0"]
+                 [org.apache.solr/solr-core "4.10.4"]
+                 [org.apache.solr/solr-solrj "4.10.4"]
                  [javax.servlet/servlet-api "2.5"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]
                                   [org.slf4j/slf4j-log4j12 "1.7.7"]]
