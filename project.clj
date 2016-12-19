8edeffedab215b5049627cc471c568d5a12c86db
Chris Hapgood <cch1@hapgoods.com>
2016-12-19 11:54:20 -0500

    Update dependencies, loosen direct plugin requirements

--- project.clj
+++ project.clj
@@ -9,11 +9,9 @@
                   ["deploy"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.apache.solr/solr-core "5.4.0"]
-                 [org.apache.solr/solr-solrj "5.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]
-                                  [org.slf4j/slf4j-log4j12 "1.7.7"]
-                                  [commons-logging "1.1.3"]]
-                   :resource-paths ["dev-resources"]
-                   :plugins [[lein-midje "3.1.1"]]}})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.apache.solr/solr-core "5.4.0" :exclusions [joda-time org.slf4j/slf4j-api]]
+                 [org.apache.solr/solr-solrj "5.4.0" :exclusions [org.slf4j/slf4j-api]]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]
+                                  [org.slf4j/slf4j-log4j12 "1.7.22"]]
+                   :resource-paths ["dev-resources"]}})
