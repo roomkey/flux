fc1f1e1ebcab8edbdf41b60d13d64e5fef832d20
Chris Hapgood <cch1@hapgoods.com>
2016-12-19 11:53:52 -0500

    Update lein-v and ensure repo is available

--- project.clj
+++ project.clj
@@ -1,7 +1,12 @@
 (defproject com.roomkey/flux :lein-v
   :description "A clojure client library for Solr"
   :url "https://github.com/roomkey/flux"
-  :plugins [[com.roomkey/lein-v "3.3.4"]]
+  :plugins [[com.roomkey/lein-v "6.0.2"]]
+  :repositories {"rk-public" {:url "http://rk-maven-public.s3-website-us-east-1.amazonaws.com/releases/"}}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["v" "update"] ;; compute new version & tag it
+                  ["vcs" "push"]
+                  ["deploy"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
