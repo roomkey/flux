fad228e1bd00c66c34d59b76c2e24b102a3c623c
Adam Frey <adam@adamfrey.me>
2017-08-02 14:48:14 -0400

    Replace the flux.core ns witha deprecated version

---
+++ src/flux/core.clj
@@ -0,0 +1,26 @@
+(ns {:deprecated true}
+  flux.core
+  "Deprecated namespace for using the flux client functions in a
+  with-connnection binding."
+  (:require [flux.client]))
+
+(def ^:dynamic *connection*)
+
+(defmacro with-connection [connection & body]
+  `(binding [*connection* ~connection]
+     ~@body))
+
+(defmacro create-fn [name]
+  `(defn ^:deprecated ~name [& args#]
+     (apply (ns-resolve 'flux.client '~name) *connection* args#)))
+
+(create-fn ping)
+(create-fn add)
+(create-fn query)
+(create-fn request)
+(create-fn commit)
+(create-fn optimize)
+(create-fn rollback)
+(create-fn delete-by-id)
+(create-fn delete-by-query)
+(create-fn shutdown)
