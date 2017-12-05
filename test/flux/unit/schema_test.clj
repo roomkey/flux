d0f1fafd180689bb9195468483ca3fb08e52c677
Adam Frey <adam@adamfrey.me>
2017-12-05 15:02:24 -0500

    Fix broken test by passing correct solr core connection

--- test/flux/unit/schema_test.clj
+++ test/flux/unit/schema_test.clj
@@ -2,6 +2,7 @@
   (:require [flux.schema :as schema]
             [flux.embedded :as e]
             [flux.spec :as spec]
+            [clojure.test :refer :all]
             [clojure.spec.alpha :as s]
             [clojure.test.check.clojure-test :refer :all]
             [clojure.test.check.properties :as prop]))
@@ -11,7 +12,7 @@
 
 (defspec add-fields-to-schema 10
   (prop/for-all [inputs (s/gen (s/coll-of ::spec/field-attributes))]
-    (let [{:keys [core core-container]} (e/create-dev-core "core1")]
-      (schema/add-fields core inputs)
-      (let [fields (set (keys (schema/get-fields (get-core core-container))))]
-        (= fields (set (map :name inputs)))))))
+                (let [{:keys [core core-container conn]} (e/create-dev-core "core1")]
+                  (schema/add-fields conn inputs)
+                  (let [fields (set (keys (schema/get-fields (get-core core-container))))]
+                    (= fields (set (map :name inputs)))))))
